05e9d666942e2c6062e66dcf1992989d82ff902d
limor-gs <limor@gigaspaces.com>
2014-11-25 22:14:41 +0200

    Bump version to 3.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-amqp-influxdb',
-    version='3.1ga',
+    version='3.1',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['amqp_influxdb'],
