cdb599d6f08c63cc482f6e8f3207b66d729e165c
opencm <limor@gigaspaces.com>
2015-12-14 13:05:04 +0200

    Bump version to 3.3.1

--- packaging/amqpinflux/provision.sh
+++ packaging/amqpinflux/provision.sh
@@ -35,10 +35,10 @@ function upload_to_s3() {
 }
 
 # VERSION/PRERELEASE/BUILD are exported to follow with our standard of exposing them as env vars. They are not used.
-export VERSION="3.3.0"
-export PRERELEASE="ga"
-export BUILD="300"
-CORE_TAG_NAME="3.3"
+export VERSION="3.3.1"
+export PRERELEASE="sp"
+export BUILD="310"
+CORE_TAG_NAME="3.3.1"
 
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
