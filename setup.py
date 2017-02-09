a375382b3782d2901a3beff781c278640e327cf0
opencm <limor@gigaspaces.com>
2017-02-09 17:31:34 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-amqp-influxdb',
-    version='4.0a13',
+    version='4.0a14',
     author='Cloudify',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['amqp_influxdb'],
